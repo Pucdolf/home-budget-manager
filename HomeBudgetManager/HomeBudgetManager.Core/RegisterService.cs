7c4929ef0ade107e34bfc417aca2c130f7d9dc35
Igor Szyk <146726015+iSheek@users.noreply.github.com>
2025-12-08 00:38:09 +0100

    [#2] Implement registration backend (#19)
    * feat(#2): add backend for create account button
    
    * feat(#2): add basic registration backend
    
    ---------
    
    Co-authored-by: Jakub <146725845+Pucdolf@users.noreply.github.com>

---
+++ HomeBudgetManager/HomeBudgetManager.Core/RegisterService.cs
@@ -0,0 +1,37 @@
+﻿namespace HomeBudgetManager.Core;
+
+
+
+// Simple register class containing creating new user and checking whether username is taken
+public class RegisterService
+{
+
+    private string path = Path.Combine(Directory.GetCurrentDirectory(), "registerData.txt");
+
+    public bool isRegistered(string username)
+    {
+        if (File.Exists(path))
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                    if (line.Contains(username))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public void registerUser(string email, string username, string password)
+    {
+        File.AppendAllText(path, email + ";" + username + ";" + password + ";");
+    }
+
+}
