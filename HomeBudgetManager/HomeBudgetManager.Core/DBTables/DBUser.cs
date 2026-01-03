e63d725bbfd247653de2466dc3be32c435fba28e
Pucdolf <jakub.jh@interia.pl>
2026-01-03 16:12:28 +0100

    feat(#10): add SystemRole enum to introduce user roles for household and system

--- HomeBudgetManager/HomeBudgetManager.Core/DBTables/DBUser.cs
+++ HomeBudgetManager/HomeBudgetManager.Core/DBTables/DBUser.cs
@@ -4,6 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeBudgetManager.Core.DBTables
 {
+    public enum SystemRole
+    {
+        Guest = 0,            // Nowo zarejestrowany, nie należy do żadnej grupy
+        Individual = 1,       // Korzysta bez grupy (tryb indywidualny)
+        HouseholdAdmin = 2,   // Twórca domu, zarządza członkami
+        HouseholdMember = 3,  // Zwykły członek domu
+        SystemAdmin = 4       // Globalny administrator systemu (zarządza aplikacją)
+    }
+
     [Index(nameof(user_email), IsUnique = true)]
     [Table("users")]
     public class DBUser
@@ -16,6 +25,8 @@ namespace HomeBudgetManager.Core.DBTables
 
         [Required]
         public string user_password { get; set; }
+        
+        public SystemRole Role { get; set; } = SystemRole.Guest;
 
         [Column("user_house_id")]
         public int? DBHouseId { get; set; } 
