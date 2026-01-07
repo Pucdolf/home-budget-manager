77e2c455ef51161206e0199cd8449f0c4f659c3b
Pucdolf <jakub.jh@interia.pl>
2026-01-07 22:22:29 +0100

    commit do rebase - kalendarz dziala

---
+++ HomeBudgetManager/HomeBudgetManager.Web/appMaps/CalendarEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using HomeBudgetManager.Core;
+
+namespace HomeBudgetManager.Web.appMaps
+{
+    public class CalendarEndpoint : IEndpoint
+    {
+        public void Map(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/calendar", async (HttpContext context, IWebHostEnvironment env, AppDbContext db) =>
+            {
+                if (!context.Request.Cookies.ContainsKey("logged_user"))
+                    return Results.Redirect("/");
+
+                var username = context.Request.Cookies["logged_user"].ToString();
+                var user = await db.Users.FirstOrDefaultAsync(u => u.Login == username);
+                if (user == null)
+                    return Results.Redirect("/");
+
+                var filePath = Path.Combine(env.WebRootPath, "calendar", "calendar.html");
+                var html = File.ReadAllText(filePath, Encoding.UTF8);
+
+                html = html.Replace("{username}", username);
+
+                return Results.Content(html, "text/html; charset=utf-8");
+            });
+        }
+    }
+}
