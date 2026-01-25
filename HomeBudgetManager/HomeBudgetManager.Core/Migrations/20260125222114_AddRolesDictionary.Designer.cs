f34e3e140cc26ec65deaae6fec7a3fc5c0985ddc
Pucdolf <jakub.jh@interia.pl>
2026-01-25 23:24:29 +0100

    feat(#131): add user roles dictionary table with data seeding

--- HomeBudgetManager/HomeBudgetManager.Core/Migrations/20260125222114_AddRolesDictionary.Designer.cs
+++ HomeBudgetManager/HomeBudgetManager.Core/Migrations/20260125222114_AddRolesDictionary.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HomeBudgetManager.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeBudgetManager.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260125222114_AddRolesDictionary")]
+    partial class AddRolesDictionary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.11");
